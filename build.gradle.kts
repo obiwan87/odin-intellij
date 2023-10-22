a6a9f749a5f4fe293c079faba78d726dbc06966f
Antonino Simone Di Stefano <simon.distefano@gmx.de>
2023-10-22 11:12:32 +0200

    Add lombok

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,49 +1,53 @@
 plugins {
-  id("java")
-  id("org.jetbrains.kotlin.jvm") version "1.8.22"
-  id("org.jetbrains.intellij") version "1.15.0"
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "1.8.22"
+    id("org.jetbrains.intellij") version "1.15.0"
+    id("io.freefair.lombok") version "6.4.0"
 }
 
 group = "com.lasagnerd"
 version = "0.0.1"
 
 repositories {
-  mavenCentral()
+    mavenCentral()
 }
 
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-  version.set("2023.2")
-  type.set("IC") // Target IDE Platform
+    version.set("2023.2")
+    type.set("IC") // Target IDE Platform
 
-  plugins.set(listOf(/* Plugin Dependencies */))
+    plugins.set(listOf(/* Plugin Dependencies */))
 }
 
 sourceSets.main.get().java.srcDirs("src/main/gen")
+dependencies {
+    implementation("org.projectlombok:lombok:1.18.28")
+}
 
 tasks {
-  // Set the JVM compatibility versions
-  withType<JavaCompile> {
-    sourceCompatibility = "17"
-    targetCompatibility = "17"
-  }
-  withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
-  }
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
+    }
 
-  patchPluginXml {
-    sinceBuild.set("232")
-    untilBuild.set("232.*")
-  }
+    patchPluginXml {
+        sinceBuild.set("232")
+        untilBuild.set("232.*")
+    }
 
-  signPlugin {
-    certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-    privateKey.set(System.getenv("PRIVATE_KEY"))
-    password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-  }
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
 
-  publishPlugin {
-    token.set(System.getenv("PUBLISH_TOKEN"))
-  }
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
 }
