0b4dfaafec3875329384579e45b0310ae7648ad1
Antonino Simone Di Stefano <simon.distefano@gmx.de>
2023-10-24 23:39:39 +0200

    Validate run configuration by checking whether SDK path is set

--- src/main/java/com/lasagnerd/odin/runConfiguration/OdinRunConfiguration.java
+++ src/main/java/com/lasagnerd/odin/runConfiguration/OdinRunConfiguration.java
@@ -9,16 +9,48 @@ import com.intellij.execution.process.ProcessHandlerFactory;
 import com.intellij.execution.process.ProcessTerminatedListener;
 import com.intellij.execution.runners.ExecutionEnvironment;
 import com.intellij.openapi.options.SettingsEditor;
+import com.intellij.openapi.options.ShowSettingsUtil;
 import com.intellij.openapi.project.Project;
 import com.lasagnerd.odin.sdkConfig.OdinSdkConfigPersistentState;
+import com.lasagnerd.odin.sdkConfig.OdinSdkConfigurable;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class OdinRunConfiguration extends RunConfigurationBase<OdinRunConfigurationOptions> {
-    protected OdinRunConfiguration(@NotNull Project project,
-                                   @Nullable ConfigurationFactory factory,
-                                   @Nullable String name) {
+    protected OdinRunConfiguration(@NotNull Project project, @Nullable ConfigurationFactory factory, @Nullable String name) {
         super(project, factory, name);
+    }
+
+    @Override
+    public void checkConfiguration() throws RuntimeConfigurationException {
+        String projectDirectoryPath = getOptions().getProjectDirectoryPath();
+
+        if (projectDirectoryPath == null || projectDirectoryPath.isEmpty()) {
+            throw new RuntimeConfigurationError("Project directory path is not set");
+        }
+
+        File file = new File(projectDirectoryPath);
+        if (!file.exists()) {
+            throw new RuntimeConfigurationError("Project directory does not exist");
+        }
+
+        String sdkPath = OdinSdkConfigPersistentState.getInstance(getProject()).getSdkPath();
+
+        if (sdkPath == null || sdkPath.isEmpty()) {
+            throw new RuntimeConfigurationError("Odin SDK path is not set",
+                    () -> ShowSettingsUtil.getInstance().showSettingsDialog(getProject(),
+                            OdinSdkConfigurable.class, null));
+        }
+
+        File sdkFile = new File(sdkPath);
+        if (!sdkFile.exists()) {
+            throw new RuntimeConfigurationError("Odin SDK path does not exist");
+        }
     }
 
     @Override
@@ -33,20 +65,35 @@ public class OdinRunConfiguration extends RunConfigurationBase<OdinRunConfigurat
     }
 
     @Override
-    public @Nullable RunProfileState getState(@NotNull Executor executor, @NotNull ExecutionEnvironment environment) throws ExecutionException {
+    public @Nullable RunProfileState getState(@NotNull Executor executor, @NotNull ExecutionEnvironment environment) {
         return new CommandLineState(environment) {
             @Override
             protected @NotNull ProcessHandler startProcess() throws ExecutionException {
-                String sdkPath = OdinSdkConfigPersistentState.getInstance().getSdkPath();
+
+                List<String> command = new ArrayList<>();
+                String sdkPath = OdinSdkConfigPersistentState.getInstance(getProject()).getSdkPath();
                 String projectDirectoryPath = getOptions().getProjectDirectoryPath();
+
                 String compilerPath = sdkPath + "/odin";
 
-                GeneralCommandLine commandLine = new GeneralCommandLine(compilerPath, "run", projectDirectoryPath, getOptions().getCompilerOptions());
+                String compilerOptions = Objects.requireNonNullElse(getOptions().getCompilerOptions(), "");
+
+                addCommandPart(command, compilerPath);
+                addCommandPart(command, "run");
+                addCommandPart(command, projectDirectoryPath);
+                addCommandPart(command, compilerOptions);
+
+                GeneralCommandLine commandLine = new GeneralCommandLine(command);
                 commandLine.setWorkDirectory(getProject().getBasePath());
-                OSProcessHandler processHandler = ProcessHandlerFactory.getInstance()
-                        .createColoredProcessHandler(commandLine);
+                OSProcessHandler processHandler = ProcessHandlerFactory.getInstance().createColoredProcessHandler(commandLine);
                 ProcessTerminatedListener.attach(processHandler);
                 return processHandler;
+            }
+
+            public void addCommandPart(List<String> list, String item) {
+                if (item != null && !item.isEmpty()) {
+                    list.add(item);
+                }
             }
         };
     }
