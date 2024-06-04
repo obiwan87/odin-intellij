7e491a20f2c74619c5cfdd07199051816c8d83e2
Antonino Simone Di Stefano <simon.distefano@gmx.de>
2024-06-04 21:00:02 +0200

    Add missing declarations to OdinSymbolResolver

--- src/main/java/com/lasagnerd/odin/codeInsight/OdinSymbolResolver.java
+++ src/main/java/com/lasagnerd/odin/codeInsight/OdinSymbolResolver.java
@@ -1,6 +1,5 @@
 package com.lasagnerd.odin.codeInsight;
 
-import com.intellij.psi.util.PsiTreeUtil;
 import com.lasagnerd.odin.lang.psi.*;
 import org.jetbrains.annotations.NotNull;
 
@@ -32,6 +31,7 @@ public class OdinSymbolResolver extends OdinVisitor {
         OdinSymbolResolver odinSymbolResolver = new OdinSymbolResolver(defaultVisibility, scope);
         odinDeclaration.accept(odinSymbolResolver);
         if (odinSymbolResolver.symbols.isEmpty()) {
+            System.out.println("No symbols found for declaration with type " + odinDeclaration.getClass().getSimpleName());
             for (OdinDeclaredIdentifier declaredIdentifier : odinDeclaration.getDeclaredIdentifiers()) {
                 OdinSymbol odinSymbol = new OdinSymbol(declaredIdentifier, defaultVisibility);
                 odinSymbolResolver.symbols.add(odinSymbol);
@@ -60,20 +60,40 @@ public class OdinSymbolResolver extends OdinVisitor {
             odinSymbol.setValueExpression(valueExpression);
             odinSymbol.setPsiType(type);
             odinSymbol.setHasUsing(using);
-
             symbols.add(odinSymbol);
+        }
+
+        if(o.getDeclaredIdentifiers().size() == 1) {
+            if(type != null) {
+                symbols.addAll(OdinInsightUtils.getTypeSymbols(type, scope));
+            } else {
+                symbols.addAll(OdinInsightUtils.getTypeSymbols(o.getExpression(), scope));
+            }
         }
     }
 
     @Override
     public void visitParameterDeclarator(@NotNull OdinParameterDeclarator o) {
         OdinType psiType = o.getTypeDefinition();
+        boolean hasUsing = false;
         for (OdinParameter odinParameter : o.getParameterList()) {
             OdinDeclaredIdentifier declaredIdentifier = odinParameter.getDeclaredIdentifier();
             OdinSymbol symbol = new OdinSymbol(declaredIdentifier);
-            symbol.setHasUsing(odinParameter.getUsing() != null);
+            hasUsing |= odinParameter.getUsing() != null;
+
+            // TODO why does every parameter have to option of having a "using"?
+            symbol.setHasUsing(hasUsing);
             symbol.setPsiType(psiType);
+
             symbols.add(symbol);
+        }
+
+        if(hasUsing) {
+            if(o.getParameterList().size() == 1) {
+                if(psiType != null) {
+                    symbols.addAll(OdinInsightUtils.getTypeSymbols(psiType, scope));
+                }
+            }
         }
     }
 
@@ -88,6 +108,12 @@ public class OdinSymbolResolver extends OdinVisitor {
             odinSymbol.setAttributeStatements(o.getAttributeStatementList());
             symbols.add(odinSymbol);
         }
+
+        if(hasUsing) {
+            if(o.getDeclaredIdentifiers().size() == 1) {
+                symbols.addAll(OdinInsightUtils.getTypeSymbols(o.getType(), scope));
+            }
+        }
     }
 
     @Override
@@ -101,17 +127,23 @@ public class OdinSymbolResolver extends OdinVisitor {
                 OdinExpression odinExpression = expressionsList.getExpressionList().get(i);
                 odinSymbol.setValueExpression(odinExpression);
             }
+
             odinSymbol.setHasUsing(hasUsing);
-            if (typeDefinition != null)
-                odinSymbol.setPsiType(typeDefinition);
+            odinSymbol.setPsiType(typeDefinition);
 
             symbols.add(odinSymbol);
         }
-    }
 
-    @Override
-    public void visitPackageDeclaration(@NotNull OdinPackageDeclaration o) {
-        super.visitPackageDeclaration(o);
+        if(hasUsing) {
+            if(o.getDeclaredIdentifiers().size() == 1 && o.getExpressionsList().getExpressionList().size() == 1) {
+                if(o.getType() != null) {
+                    symbols.addAll(OdinInsightUtils.getTypeSymbols(o.getType(), scope));
+                } else {
+                    OdinExpression odinExpression = o.getExpressionsList().getExpressionList().get(0);
+                    symbols.addAll(OdinInsightUtils.getTypeSymbols(odinExpression, scope));
+                }
+            }
+        }
     }
 
     @Override
@@ -172,6 +204,52 @@ public class OdinSymbolResolver extends OdinVisitor {
     public void visitPolymorphicType(@NotNull OdinPolymorphicType o) {
         OdinSymbol odinSymbol = new OdinSymbol(o.getDeclaredIdentifier(), OdinSymbol.OdinVisibility.LOCAL);
         odinSymbol.setSymbolType(OdinSymbol.OdinSymbolType.POLYMORPHIC_TYPE);
+        symbols.add(odinSymbol);
+    }
+
+    @Override
+    public void visitEnumDeclarationStatement(@NotNull OdinEnumDeclarationStatement o) {
+        OdinSymbol odinSymbol = new OdinSymbol(o.getDeclaredIdentifier(), getVisibility(o.getAttributeStatementList(), defaultVisibility));
+        odinSymbol.setSymbolType(OdinSymbol.OdinSymbolType.ENUM);
+        odinSymbol.setAttributeStatements(o.getAttributeStatementList());
+        odinSymbol.setPsiType(o.getEnumType());
+        symbols.add(odinSymbol);
+    }
+
+    @Override
+    public void visitBitsetDeclarationStatement(@NotNull OdinBitsetDeclarationStatement o) {
+        OdinSymbol odinSymbol = new OdinSymbol(o.getDeclaredIdentifier(), getVisibility(o.getAttributeStatementList(), defaultVisibility));
+        odinSymbol.setPsiType(o.getBitSetType());
+        odinSymbol.setSymbolType(OdinSymbol.OdinSymbolType.BIT_SET);
+        odinSymbol.setAttributeStatements(o.getAttributeStatementList());
+        symbols.add(odinSymbol);
+    }
+
+    @Override
+    public void visitConstantInitializationStatement(@NotNull OdinConstantInitializationStatement o) {
+        OdinType typeDefinition = o.getType();
+        for (int i = 0; i < o.getDeclaredIdentifiers().size(); i++) {
+            OdinSymbol odinSymbol = new OdinSymbol(o.getDeclaredIdentifiers().get(i));
+            OdinExpressionsList expressionsList = o.getExpressionsList();
+            if (expressionsList.getExpressionList().size() > i) {
+                OdinExpression odinExpression = expressionsList.getExpressionList().get(i);
+                odinSymbol.setValueExpression(odinExpression);
+            }
+
+            odinSymbol.setHasUsing(false);
+            odinSymbol.setPsiType(typeDefinition);
+            odinSymbol.setSymbolType(OdinSymbol.OdinSymbolType.CONSTANT);
+            symbols.add(odinSymbol);
+        }
+
+    }
+
+    @Override
+    public void visitUnionDeclarationStatement(@NotNull OdinUnionDeclarationStatement o) {
+        OdinSymbol odinSymbol = new OdinSymbol(o.getDeclaredIdentifier(), getVisibility(o.getAttributeStatementList(), defaultVisibility));
+        odinSymbol.setPsiType(o.getUnionType());
+        odinSymbol.setAttributeStatements(o.getAttributeStatementList());
+        odinSymbol.setSymbolType(OdinSymbol.OdinSymbolType.UNION);
         symbols.add(odinSymbol);
     }
 
