5788bab07b49f72c44856abb8c6ac6a04d18afb7
Dima Butemann <dbutemann@gmail.com>
2024-08-14 22:18:03 +0200

    Improve isPairedBracesAllowedBeforeType and reformat

--- src/main/java/com/lasagnerd/odin/formatting/OdinBraceMatcher.java
+++ src/main/java/com/lasagnerd/odin/formatting/OdinBraceMatcher.java
@@ -4,6 +4,7 @@ import com.intellij.lang.BracePair;
 import com.intellij.lang.PairedBraceMatcher;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.TokenType;
 import com.intellij.psi.tree.IElementType;
 import com.lasagnerd.odin.lang.psi.OdinTypes;
 import org.jetbrains.annotations.NotNull;
@@ -13,7 +14,7 @@ public class OdinBraceMatcher implements PairedBraceMatcher {
 
     @Override
     public BracePair @NotNull [] getPairs() {
-        return new BracePair[] {
+        return new BracePair[]{
                 new BracePair(OdinTypes.LPAREN, OdinTypes.RPAREN, false),
                 new BracePair(OdinTypes.LBRACKET, OdinTypes.RBRACKET, false),
                 new BracePair(OdinTypes.LBRACE, OdinTypes.RBRACE, false),
@@ -22,6 +23,12 @@ public class OdinBraceMatcher implements PairedBraceMatcher {
 
     @Override
     public boolean isPairedBracesAllowedBeforeType(@NotNull IElementType lbraceType, @Nullable IElementType context) {
+        if (context == null) {
+            return true;
+        }
+        if (lbraceType.equals(OdinTypes.LPAREN)) {
+            return context == TokenType.WHITE_SPACE || context == OdinTypes.NEW_LINE;
+        }
         return true;
     }
 
@@ -29,7 +36,7 @@ public class OdinBraceMatcher implements PairedBraceMatcher {
     public int getCodeConstructStart(PsiFile psiFile, int offset) {
         PsiElement psiElement = psiFile.findElementAt(offset);
 
-        if(psiElement != null && psiElement.getParent() != null) {
+        if (psiElement != null && psiElement.getParent() != null) {
             return psiElement.getParent().getTextRange().getStartOffset();
         }
         return offset;
