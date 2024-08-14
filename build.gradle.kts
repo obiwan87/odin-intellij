1e893cde27419c4651cbdf193a79f5ee2d603eb6
Simone Di Stefano <simone.distefano@merkurtz.info>
2024-08-14 17:38:44 +0200

    Bump up version to 0.5.1 and update changelog

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "com.lasagnerd"
-version = "0.5.0"
+version = "0.5.1"
 
 repositories {
     mavenCentral()
@@ -23,7 +23,7 @@ intellijPlatform  {
     projectName = project.name
 
     pluginConfiguration {
-        version = "0.5.0"
+        version = "0.5.1"
 
         ideaVersion {
             sinceBuild = "242"
