e95e78aa2ab37985acadeda047d0cb33dec27e8f
Antonino Simone Di Stefano <simon.distefano@gmx.de>
2024-08-15 16:15:51 +0200

    Add notification for missing SDK

---
+++ src/main/java/com/lasagnerd/odin/sdkConfig/OdinEditorNotificationProvider.java
@@ -0,0 +1,38 @@
+package com.lasagnerd.odin.sdkConfig;
+
+import com.intellij.ide.IdeBundle;
+import com.intellij.openapi.fileEditor.FileEditor;
+import com.intellij.openapi.options.ShowSettingsUtil;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.ui.EditorNotificationPanel;
+import com.intellij.ui.EditorNotificationProvider;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.util.Optional;
+import java.util.function.Function;
+
+public class OdinEditorNotificationProvider implements EditorNotificationProvider {
+    @Override
+    public @Nullable Function<? super @NotNull FileEditor, ? extends @Nullable JComponent> collectNotificationData(@NotNull Project project, @NotNull VirtualFile file) {
+        return fileEditor -> {
+            Optional<String> sdkPath = OdinSdkConfigPersistentState.getSdkPath(project);
+            if(sdkPath.isPresent())
+                return null;
+            EditorNotificationPanel panel = new EditorNotificationPanel(fileEditor, EditorNotificationPanel.Status.Warning);
+            panel.setText("No Odin SDK has been setup for this project");
+            panel.createActionLabel("Setup SDK", () -> {
+                if (!project.isDisposed()) {
+                    file.refresh(false, false);
+                    ShowSettingsUtil.getInstance().showSettingsDialog(project,
+                            OdinSdkConfigurable.class, null);
+                }
+            });
+            return panel;
+        };
+    }
+
+
+}
