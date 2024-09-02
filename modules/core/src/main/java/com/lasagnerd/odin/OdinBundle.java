6bebb8debb87b714b4534fbf70458c75352422b4
Antonino Simone Di Stefano <simon.distefano@gmx.de>
2024-09-02 19:36:40 +0200

    Add Bundle for NLS strings

---
+++ modules/core/src/main/java/com/lasagnerd/odin/OdinBundle.java
@@ -0,0 +1,19 @@
+package com.lasagnerd.odin;
+
+import com.intellij.DynamicBundle;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.PropertyKey;
+
+public class OdinBundle extends DynamicBundle {
+    public static final String ODIN_BUNDLE = "odin.Bundle";
+    public static final OdinBundle INSTANCE = new OdinBundle();
+    protected OdinBundle() {
+        super(ODIN_BUNDLE);
+    }
+
+    @Nls
+    public static String message(@PropertyKey(resourceBundle = ODIN_BUNDLE) String key, Object... params) {
+        return INSTANCE.getMessage(key, params);
+    }
+}
