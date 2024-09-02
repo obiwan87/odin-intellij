fb66343116970c7fa48518f2efbea601ddb46d70
Antonino Simone Di Stefano <simon.distefano@gmx.de>
2024-09-02 10:12:15 +0200

    Add WinDAP driver

---
+++ modules/debugger/src/main/java/com/lasagnerd/odin/debugger/drivers/windap/WinDAPDriver.java
@@ -0,0 +1,186 @@
+/*
+ * Copyright 2023-2024 FalsePattern
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lasagnerd.odin.debugger.drivers.windap;
+
+import com.intellij.execution.ExecutionException;
+import com.intellij.util.system.CpuArch;
+import com.jetbrains.cidr.ArchitectureType;
+import com.lasagnerd.odin.debugger.drivers.dap.DAPDebuggerDriverConfiguration;
+import com.lasagnerd.odin.debugger.drivers.dap.DAPDriver;
+import com.lasagnerd.odin.debugger.drivers.dap.WrappedDebugServer;
+import lombok.*;
+import org.eclipse.lsp4j.debug.Capabilities;
+import org.eclipse.lsp4j.debug.OutputEventArguments;
+import org.eclipse.lsp4j.debug.services.IDebugProtocolServer;
+import org.eclipse.lsp4j.debug.util.ToStringBuilder;
+import org.eclipse.lsp4j.jsonrpc.JsonRpcException;
+import org.eclipse.lsp4j.jsonrpc.MessageConsumer;
+import org.eclipse.lsp4j.jsonrpc.MessageIssueException;
+import org.eclipse.lsp4j.jsonrpc.debug.messages.DebugResponseMessage;
+import org.eclipse.lsp4j.jsonrpc.messages.Message;
+import org.eclipse.lsp4j.jsonrpc.services.JsonRequest;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
+public class WinDAPDriver extends DAPDriver<
+        IDebugProtocolServer, WrappedDebugServer<IDebugProtocolServer>,
+        WinDAPDriver.WinDAPDebuggerClient
+        > {
+    private final CompletableFuture<HandshakeResponse> handshakeFuture = new CompletableFuture<>();
+    private final HandshakeStrategy handshakeStrategy;
+
+    public WinDAPDriver(HandshakeStrategy handshakeStrategy, @NotNull Handler handler, DAPDebuggerDriverConfiguration config) throws ExecutionException {
+        super(handler, config);
+        this.handshakeStrategy = handshakeStrategy;
+        DAPDriver$postConstructor();
+    }
+
+    @Override
+    protected @NotNull String getClientName() {
+        return "Odin";
+    }
+
+    @Override
+    protected @NotNull String getClientID() {
+        return "win-odin";
+    }
+
+    @Override
+    public void DAPDriver$postConstructor$invoke() {
+
+    }
+
+    @Override
+    protected MessageConsumer wrapMessageConsumer(MessageConsumer messageConsumer) {
+        return new MessageConsumer() {
+            private boolean verifyHandshake = true;
+
+            @Override
+            public void consume(Message message) throws MessageIssueException, JsonRpcException {
+                if (verifyHandshake && message instanceof DebugResponseMessage res && res.getMethod().equals("handshake")) {
+                    verifyHandshake = false;
+                    res.setResponseId(1);
+                }
+                messageConsumer.consume(message);
+            }
+        };
+    }
+
+    @Override
+    protected Class<IDebugProtocolServer> getServerInterface() {
+        return IDebugProtocolServer.class;
+    }
+
+    @Override
+    protected WrappedDebugServer<IDebugProtocolServer> wrapDebugServer(IDebugProtocolServer remoteProxy) {
+        return new WrappedDebugServer<>(remoteProxy);
+    }
+
+    @Override
+    protected @NotNull String getType() {
+        return "cppvsdbg";
+    }
+
+    @Override
+    protected void addArgsForLaunch(Map<String, Object> args) {
+
+        args.put("console", "integratedTerminal");
+        args.put("logging", Map.of("moduleLoad", true));
+        args.put("__configurationTarget", 2);
+    }
+
+    @Override
+    protected WinDAPDebuggerClient createDebuggerClient() {
+        return this.new WinDAPDebuggerClient();
+    }
+
+    @Override
+    protected CompletableFuture<?> wrapInitialize(CompletableFuture<Capabilities> capabilitiesCompletableFuture) {
+        return capabilitiesCompletableFuture.thenCombine(handshakeFuture, (res, hs) -> res);
+    }
+
+
+    @Override
+    protected @NotNull String functionParser(String name) {
+        return name.substring(name.indexOf('!') + 1, name.indexOf('('));
+    }
+
+    protected class WinDAPDebuggerClient extends DAPDebuggerClient {
+        @Override
+        public void output(OutputEventArguments args) {
+            if ("telemetry".equals(args.getCategory())) {
+                return;
+            }
+            super.output(args);
+        }
+
+
+        @JsonRequest
+        public CompletableFuture<HandshakeResponse> handshake(HandshakeRequest request) {
+            return CompletableFuture
+                    .supplyAsync(() -> handshakeStrategy.handshake(request))
+                    .thenApply(handshakeResponse -> {
+                        handshakeFuture.complete(handshakeResponse);
+                        return handshakeResponse;
+                    });
+        }
+    }
+
+
+    @Override
+    public @Nullable String getArchitecture() {
+        return ArchitectureType.forVmCpuArch(CpuArch.CURRENT).getId();
+    }
+
+    @Data
+    @NoArgsConstructor
+    public static class HandshakeRequest {
+        @NonNull
+        private String value;
+
+        @Override
+        public String toString() {
+            ToStringBuilder b = new ToStringBuilder(this);
+            b.add("value", this.value);
+            return b.toString();
+        }
+    }
+
+    @Data
+    @NoArgsConstructor
+    @RequiredArgsConstructor
+    public static class HandshakeResponse {
+        @NonNull
+        private String signature;
+
+        @Override
+        public String toString() {
+            ToStringBuilder b = new ToStringBuilder(this);
+            b.add("signature", this.signature);
+            return b.toString();
+        }
+    }
+
+    @FunctionalInterface
+    public interface HandshakeStrategy {
+        WinDAPDriver.HandshakeResponse handshake(WinDAPDriver.HandshakeRequest request);
+    }
+
+}
--- modules/debugger/src/main/java/com/lasagnerd/odin/debugger/toolchains/WinDAPToolchain.java
+++ modules/debugger/src/main/java/com/lasagnerd/odin/debugger/toolchains/WinDAPToolchain.java
@@ -1,8 +1,20 @@
 package com.lasagnerd.odin.debugger.toolchains;
 
+import com.jetbrains.cidr.execution.debugger.backend.DebuggerDriverConfiguration;
+import com.lasagnerd.odin.debugger.drivers.windap.WinDAPDriver;
+import com.lasagnerd.odin.debugger.drivers.windap.WinDAPDriverConfiguration;
 import com.lasagnerd.odin.extensions.OdinDebuggerToolchain;
+import org.jetbrains.annotations.NotNull;
 
-public class WinDAPToolchain implements OdinDebuggerToolchain {
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.security.MessageDigest;
+import java.util.Base64;
+import java.util.zip.Inflater;
+
+public class WinDAPToolchain implements OdinDebuggerToolchain, DebuggerDriverConfigurationProvider {
     @Override
     public String getId() {
         return "odin-win-db-dap";
@@ -33,5 +45,43 @@ public class WinDAPToolchain implements OdinDebuggerToolchain {
     @Override
     public String getVersion() {
         return "";
+    }
+
+    @Override
+    public DebuggerDriverConfiguration getDebuggerDriverConfiguration(String path) {
+        return new WinDAPDriverConfiguration(Path.of(path), this::handshakeAlgorithm);
+    }
+
+    private @NotNull WinDAPDriver.HandshakeResponse handshakeAlgorithm(WinDAPDriver.HandshakeRequest handshake) {
+        try {
+            final MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+            messageDigest.update(handshake.getValue().getBytes(StandardCharsets.UTF_8));
+            Inflater inflater = new Inflater(true);
+            final byte[] odinsGift = prayToOdin();
+            inflater.setInput(odinsGift, odinsGift.length - 0x80, 77);
+            inflater.finished();
+            byte[] b = new byte[1];
+            while (inflater.inflate(b) > 0) {
+                messageDigest.update(b);
+            }
+
+            return new WinDAPDriver.HandshakeResponse(new String(odinsGift,
+                    odinsGift.length - 0x33,
+                    3) + Base64.getEncoder().encodeToString(messageDigest.digest()));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static byte[] prayToOdin() throws IOException {
+        final byte[] wanderer;
+        try (InputStream inputStream = WinDAPToolchain.class.getResourceAsStream("/wanderer.jpg")) {
+            if (inputStream == null) {
+                throw new RuntimeException("Odin has not answered your prayers.");
+            }
+            wanderer = inputStream.readAllBytes();
+
+        }
+        return wanderer;
     }
 }
