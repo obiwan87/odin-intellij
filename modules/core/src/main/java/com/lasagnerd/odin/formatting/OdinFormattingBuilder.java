5e0def685e29a3535fbb0b686aa437ad9ac1a835
Antonino Simone Di Stefano <simon.distefano@gmx.de>
2024-09-17 01:07:43 +0200

    Revert changes to compound value

--- modules/core/src/main/java/com/lasagnerd/odin/formatting/OdinFormattingBuilder.java
+++ modules/core/src/main/java/com/lasagnerd/odin/formatting/OdinFormattingBuilder.java
@@ -78,10 +78,10 @@ public class OdinFormattingBuilder implements FormattingModelBuilder {
                 .aroundInside(OdinTypes.EQ, OdinTypes.ENUM_BODY)
                 .spaces(1)
 
-                .afterInside(OdinTypes.BLOCK_START, OdinTypes.COMPOUND_VALUE)
+                .afterInside(OdinTypes.COMPOUND_VALUE_START, OdinTypes.COMPOUND_VALUE)
                 .spaces(1)
 
-                .beforeInside(OdinTypes.BLOCK_END, OdinTypes.COMPOUND_VALUE)
+                .beforeInside(OdinTypes.COMPOUND_VALUE_END, OdinTypes.COMPOUND_VALUE)
                 .spaces(1)
 
                 .aroundInside(BINARY_OPERATORS, BINARY_EXPRESSIONS)
@@ -169,7 +169,7 @@ public class OdinFormattingBuilder implements FormattingModelBuilder {
                 .createFormattingModelForPsiFile(formattingContext.getContainingFile(),
                         new OdinFormatterBlock(formattingContext.getNode(),
                                 Wrap.createWrap(WrapType.NONE, false),
-                                null,
+                                Alignment.createAlignment(),
                                 Indent.getNoneIndent(),
                                 createSpaceBuilder(codeStyleSettings)),
                         codeStyleSettings);
