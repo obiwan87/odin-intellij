f331abda154e8cbbf412995305a865b5dbe433ec
Antonino Simone Di Stefano <simon.distefano@gmx.de>
2024-10-09 21:19:20 +0200

    Add notification panel for when SDK path is invalid

--- modules/core/src/main/java/com/lasagnerd/odin/projectSettings/OdinEditorNotificationProvider.java
+++ modules/core/src/main/java/com/lasagnerd/odin/projectSettings/OdinEditorNotificationProvider.java
@@ -6,10 +6,12 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.ui.EditorNotificationPanel;
 import com.intellij.ui.EditorNotificationProvider;
+import com.intellij.ui.HyperlinkLabel;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
+import java.io.File;
 import java.util.Optional;
 import java.util.function.Function;
 
@@ -17,38 +19,56 @@ public class OdinEditorNotificationProvider implements EditorNotificationProvide
     @Override
     public @Nullable Function<? super @NotNull FileEditor, ? extends @Nullable JComponent> collectNotificationData(@NotNull Project project, @NotNull VirtualFile file) {
         return fileEditor -> {
+
+            String odinBinaryPath = OdinSdkUtils.getOdinBinaryPath(project);
+            if(odinBinaryPath != null) {
+                if(!new File(odinBinaryPath).exists()) {
+                    EditorNotificationPanel panel = new EditorNotificationPanel(fileEditor, EditorNotificationPanel.Status.Error);
+                    panel.setText("Odin SDK path is not valid");
+                    addOpenSettingsAction(project, panel, "Fix", file);
+                }
+            }
             if(!file.getPath().endsWith(".odin"))
                 return null;
 
             Optional<String> sdkPath = OdinSdkUtils.getSdkPath(project);
 
             if(sdkPath.isPresent()) {
-                if(!OdinSdkLibraryManager.isSdkLibraryConfigured(project)) {
-                    EditorNotificationPanel panel = new EditorNotificationPanel(fileEditor, EditorNotificationPanel.Status.Warning);
-                    panel.setText("Odin SDK is not configured as project collection");
-                    panel.createActionLabel("Set collection", () -> {
-                        if (!project.isDisposed()) {
-                            file.refresh(false, false);
-                            OdinSdkLibraryManager.addOrUpdateOdinSdkLibrary(project, sdkPath.get());
-                        }
-                    });
-                    return panel;
-                }
                 return null;
             }
 
-            EditorNotificationPanel panel = new EditorNotificationPanel(fileEditor, EditorNotificationPanel.Status.Warning);
-            panel.setText("No Odin SDK has been setup for this project");
-            panel.createActionLabel("Setup SDK", () -> {
-                if (!project.isDisposed()) {
-                    file.refresh(false, false);
-                    ShowSettingsUtil.getInstance().showSettingsDialog(project,
-                            OdinProjectConfigurable.class, null);
-                }
-            });
-
-            return panel;
+            return createSetupSdkPanel(project, file, fileEditor);
         };
+    }
+
+    private static @NotNull EditorNotificationPanel createSetupSDKasLibrary(@NotNull Project project, @NotNull VirtualFile file, FileEditor fileEditor, Optional<String> sdkPath) {
+        EditorNotificationPanel panel = new EditorNotificationPanel(fileEditor, EditorNotificationPanel.Status.Warning);
+        panel.setText("Odin SDK is not configured as project collection");
+        panel.createActionLabel("Set collection", () -> {
+            if (!project.isDisposed()) {
+                file.refresh(false, false);
+                OdinSdkLibraryManager.addOrUpdateOdinSdkLibrary(project, sdkPath.get());
+            }
+        });
+        return panel;
+    }
+
+    private static @NotNull EditorNotificationPanel createSetupSdkPanel(@NotNull Project project, @NotNull VirtualFile file, FileEditor fileEditor) {
+        EditorNotificationPanel panel = new EditorNotificationPanel(fileEditor, EditorNotificationPanel.Status.Warning);
+        panel.setText("No Odin SDK has been setup for this project");
+        String label = "Setup SDK";
+        addOpenSettingsAction(project, panel, label, file);
+        return panel;
+    }
+
+    private static void addOpenSettingsAction(@NotNull Project project, EditorNotificationPanel panel, String label, @NotNull VirtualFile file) {
+        panel.createActionLabel(label, () -> {
+            if (!project.isDisposed()) {
+                file.refresh(false, false);
+                ShowSettingsUtil.getInstance().showSettingsDialog(project,
+                        OdinProjectConfigurable.class, null);
+            }
+        });
     }
 
 
