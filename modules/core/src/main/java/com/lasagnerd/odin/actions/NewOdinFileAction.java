a7e41656c40ca76c510d77e755b694b5f761d440
Antonino Simone Di Stefano <simon.distefano@gmx.de>
2025-01-13 16:28:21 +0100

    Refine package name choice when creating a file directly under the source root

--- modules/core/src/main/java/com/lasagnerd/odin/actions/NewOdinFileAction.java
+++ modules/core/src/main/java/com/lasagnerd/odin/actions/NewOdinFileAction.java
@@ -10,19 +10,61 @@ import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.fileEditor.FileEditor;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.fileEditor.TextEditor;
+import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ModuleRootManager;
+import com.intellij.openapi.roots.ProjectFileIndex;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiFileFactory;
+import com.intellij.psi.PsiManager;
+import com.lasagnerd.odin.codeInsight.refactor.OdinNameSuggester;
 import com.lasagnerd.odin.lang.OdinFileType;
+import com.lasagnerd.odin.lang.psi.OdinFile;
+import com.lasagnerd.odin.lang.psi.OdinFileScope;
+import com.lasagnerd.odin.projectStructure.module.rootTypes.source.OdinSourceRootType;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.Objects;
 
 public class NewOdinFileAction extends AnAction {
 
     @Override
     public @NotNull ActionUpdateThread getActionUpdateThread() {
         return ActionUpdateThread.BGT;
+    }
+
+    private static String choosePackageName(PsiDirectory directory, Project project) {
+        VirtualFile directoryVirtualFile = directory.getVirtualFile();
+        ProjectFileIndex projectFileIndex = ProjectFileIndex.getInstance(project);
+        Module moduleForFile = projectFileIndex.getModuleForFile(directoryVirtualFile);
+        String packageName = directoryVirtualFile.getName();
+        if (moduleForFile != null) {
+            List<VirtualFile> sourceRoots = ModuleRootManager.getInstance(moduleForFile)
+                    .getSourceRoots(OdinSourceRootType.INSTANCE);
+            if (sourceRoots.stream().anyMatch(directoryVirtualFile::equals)) {
+                if (directoryVirtualFile.getChildren().length > 0) {
+                    for (VirtualFile child : directoryVirtualFile.getChildren()) {
+                        if (Objects.equals(child.getExtension(), "odin")) {
+                            PsiFile file = PsiManager.getInstance(project).findFile(child);
+                            if (file instanceof OdinFile odinFile) {
+                                OdinFileScope fileScope = odinFile.getFileScope();
+                                if (fileScope != null) {
+                                    packageName = fileScope.getPackageClause().getName();
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    packageName = OdinNameSuggester.normalizeName(project.getName());
+                }
+            }
+        }
+        return packageName;
     }
 
     @Override
@@ -46,15 +88,17 @@ public class NewOdinFileAction extends AnAction {
 
         final String finalFileName = fileName;
 
-        String packageName = directory.getVirtualFile().getName();
+
         if (directory.findFile(fileName) != null) {
             Messages.showErrorDialog(project, "A file with this name already exists.", "Error");
             return;
         }
 
+        final String finalPackageName = choosePackageName(directory, project);
+
         // run write action
         ApplicationManager.getApplication().invokeLater(() -> WriteCommandAction.runWriteCommandAction(project, () -> {
-            String content = "package %s\n\n".formatted(packageName);
+            String content = "package %s\n\n".formatted(finalPackageName);
             PsiFileFactory factory = PsiFileFactory.getInstance(project);
             PsiFile newFile = factory.createFileFromText(finalFileName, OdinFileType.INSTANCE, content);
             directory.add(newFile);
@@ -66,7 +110,7 @@ public class NewOdinFileAction extends AnAction {
                         .openFile(createdFile.getVirtualFile(), true);
 
                 FileEditor selectedEditor = fileEditorManager.getSelectedEditor();
-                if(selectedEditor instanceof TextEditor textEditor) {
+                if (selectedEditor instanceof TextEditor textEditor) {
                     Editor editor = textEditor.getEditor();
                     editor.getCaretModel().moveToOffset(content.length());
                 }
